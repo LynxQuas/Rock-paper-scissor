2db0f774e6ac78578736c38c4c71a39faa3a9a5e
LynxQuas <lynxquas@gmail.com>
2023-10-14 19:51:58 +0630

    added the script file

--- script.js
+++ script.js
@@ -1,1 +1,62 @@
-console.log("hello world");
+const weapon = ["🪨", "📃", "✂️"];
+let playerScore = 0;
+let computerScore = 0;
+
+function getComputerChoice() {
+    const randomWeapon = Math.floor(Math.random() * 3);
+    return weapon[randomWeapon];
+}
+
+function displaySelection(place, weapon) {
+    place.textContent = weapon;
+}
+
+const computerWeapon = document.querySelector(".computer_weapon");
+const playerWeapon = document.querySelector(".player_weapon");
+const result = document.querySelector(".battle_result");
+
+const clickHandler = function (e) {
+    if (!e.target.classList.contains("btn")) return;
+
+    const [playerSelection] = weapon.filter(
+        (weapon) => weapon === e.target.textContent
+    );
+
+    const computerSelection = getComputerChoice();
+    displaySelection(playerWeapon, playerSelection);
+    displaySelection(computerWeapon, computerSelection);
+    playRound(playerSelection, computerSelection);
+
+    if (playerScore === 5) {
+        result.classList.add("win");
+        document.removeEventListener("click", clickHandler);
+    }
+    if (computerScore === 5) {
+        result.classList.add("lose");
+        document.removeEventListener("click", clickHandler);
+    }
+};
+
+document.addEventListener("click", clickHandler);
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        result.textContent = `Tie ${playerSelection} , ${computerSelection}`;
+        return;
+    }
+
+    if (
+        (playerSelection === "🪨" && computerSelection === "📃") ||
+        (playerSelection === "📃" && computerSelection === "✂️") ||
+        (playerSelection === "✂️" && computerSelection === "🪨")
+    ) {
+        result.textContent = `You lose ${computerSelection} beats ${playerSelection}`;
+        computerScore += 1;
+        document.querySelector(".computer_score").textContent = computerScore;
+        return;
+    } else {
+        playerScore += 1;
+        document.querySelector(".player_score").textContent = playerScore;
+        result.textContent = `You win ${playerSelection} beats ${computerSelection}`;
+    }
+}
